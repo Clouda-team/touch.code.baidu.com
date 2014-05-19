cbe7b91a1b8f9d7c800f72b6d7bae8204ca5446b
huangxin03 <huangxin03@baidu.com>
2014-05-19 16:13:37 +0800

    log

--- examples.js
+++ examples.js
@@ -129,7 +129,7 @@
 					
 				    dx = dx || 0;
 					dy = dy || 0;
-				    log("当前x值为:" + dx + ", 当前y值为:" + dy +".");
+				    
 					var offx = dx + ev.x + "px";
 					var offy = dy + ev.y + "px";
 					this.style.webkitTransform = "translate3d(" + offx + "," + offy + ",0)";
@@ -139,6 +139,7 @@
 				touch.on('#target', 'dragend', function(ev){
 					dx += ev.x;
 				    dy += ev.y;
+					log("当前x值为:" + dx + ", 当前y值为:" + dy +".");
 				});
 			},
 			touch : function(){
